3ee95628cc5519b28a955ae616b7d78a26de3cb0
linyngfly <linyngfly@163.com>
2018-05-29 01:56:07 +0800

    游戏加入接口定义

--- app/servers/game/gameApp.js
+++ app/servers/game/gameApp.js
@@ -1,5 +1,6 @@
 const omelo = require('omelo');
 const { RedisConnector, MysqlConnector } = require('../../database/dbclient');
+const ERROR_OBJ = require('../../consts/error_code').ERROR_OBJ;
 
 class GameApp {
     async start() {
@@ -33,43 +34,42 @@ class GameApp {
         if(this[route]){
             return await this[route](msg, session);
         }
-        this[route](msg, session);
+        await this[route](msg, session);
     }
 
-    c_enter(msg, session, cb) {
+    c_enter(msg, session) {
         let self = this;
-        let _uid = msg.uid;
         let sessionService = omelo.app.get('sessionService');
-        async.waterfall([
-            function (cb) {
-                sessionService.kick(_uid, function (err) {
-                    if (err) {
-                        cb(CONSTS.SYS_CODE.SYSTEM_ERROR);
-                    } else {
-                        cb();
+
+        return new Promise(function (resolve, reject) {
+            sessionService.kick(msg.uid, function () {
+                session.bind(msg.uid, function (err) {
+                    if(err){
+                        reject(err);
+                        return;
                     }
+                    session.on('closed', self.close.bind(self));
+                    //TODO 处理校验玩家要加入的游戏类型是否支持
+                    resolve();
+                    logger.info(`用户[${msg.uid}]登陆成功`);
                 });
-            },
-            function (cb) {
-                session.bind(_uid, function (err) {
-                    if (err) {
-                        cb(CONSTS.SYS_CODE.SYSTEM_ERROR);
-                    } else {
-                        session.on('closed', self._socketClose.bind(self));
-                        cb();
-                    }
-                });
-            }
-        ], function (err) {
-            utils.invokeCallback(cb, err);
-            logger.info(`用户[${_uid}]登陆成功`);
+            });
         });
     }
 
-    c_leave(msg, session, cb) {
+    c_leave(msg, session) {
+        session.uid;
         let sessionService = omelo.app.get('sessionService');
         sessionService && sessionService.kick(msg.uid);
         utils.invokeCallback(cb, null);
+    }
+
+    close(session, reason) {
+        if (!session || !session.uid) {
+            return;
+        }
+        let uid = session.uid;
+        logger.info(`用户[${uid}] 网络连接断开`, reason);
     }
 }
 
