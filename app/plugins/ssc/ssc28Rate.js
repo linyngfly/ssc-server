48a53e0ae029b015f0f1cc7fd619ca1beeb00dc9
linyang <linyngfly@163.com>
2018-06-05 14:43:36 +0800

    赔率表生成

---
+++ app/plugins/ssc/ssc28Rate.js
@@ -0,0 +1,18 @@
+class SSC28Rate{
+    constructor(){
+        this._rateMap = new Map();
+    }
+
+    loadConfig(config){
+    }
+
+    getCode(type){
+
+    }
+
+    getRate(code){
+
+    }
+}
+
+module.exports = SSC28Rate;
