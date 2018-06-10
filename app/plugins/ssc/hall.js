97471d7b15693d78fd96ff38f5dc33cbcf597ff9
linyngfly <linyngfly@163.com>
2018-06-10 19:24:04 +0800

    投注完善

---
+++ app/plugins/ssc/hall.js
@@ -0,0 +1,22 @@
+
+
+class Hall{
+    async start(){
+        logger.error('Hall start');
+    }
+
+    async stop(){
+        logger.error('Hall stop');
+    }
+
+    async recharge(data){
+        let account = data.account;
+        await mysqlConnector.query('INSERT INTO `tbl_order` (`uid`, `num`, `type`, `bankInfo`,`created_at`,`state`) VALUES ()', [account.uid, ]);
+    }
+
+    async cash(data){
+
+    }
+}
+
+module.exports = Hall;
