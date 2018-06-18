3137ea888493d9bcc2bdaa2cfaa35fecb23528b8
linyngfly <linyngfly@163.com>
2018-06-19 07:35:01 +0800

    玩家返水、拉手分成

--- app/models/wzgj/account/index.js
+++ app/models/wzgj/account/index.js
@@ -3,3 +3,4 @@ module.exports ={
     fieldConst:require('./accountFieldConst'),
     modelDefine:require('./accountModel'),
 };
+
--- app/plugins/ssc/lucky28/lucky28Income.js
+++ app/plugins/ssc/lucky28/lucky28Income.js
@@ -135,6 +135,9 @@ class Lucky28Income {
                     dayIncomeInfo.defectionMoney = defectionMoney;
                     dayIncomeInfo.defectionRate = defectionRate;
                     logBuilder.addLog(logBuilder.tbl_id.TBL_PLAYER_INCOME, dayIncomeInfo);
+                    let account = await models.account.helper.getAccount(uid, models.account.fieldConst.MONEY);
+                    account.money = defectionMoney;
+                    await account.commit();
                 }
             } catch (err) {
                 err;
@@ -222,6 +225,9 @@ class Lucky28Income {
                     dayIncomeInfo.rebateMoney = rebateMoney;
                     dayIncomeInfo.rebateRate = rebateRate;
                     logBuilder.addLog(logBuilder.tbl_id.TBL_AGENT_INCOME, dayIncomeInfo);
+                    let account = await models.account.helper.getAccount(uid, models.account.fieldConst.MONEY);
+                    account.money = rebateMoney;
+                    await account.commit();
                 }
             } catch (err) {
                 err;
